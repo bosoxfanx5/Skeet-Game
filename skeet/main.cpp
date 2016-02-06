4bebc9037cc9c4dfda90190be4e9b9d008692ed9
Administrator1 <admin@owners-macbook-pro.local>
2016-02-06 10:45:41 -0800

    Initial Commit

---
+++ skeet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  skeet
+//
+//  Created by Administrator on 2/6/16.
+//  Copyright © 2016 BYU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
