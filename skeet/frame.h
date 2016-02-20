e29a96b82370c2407ecb95e11b793f5772835fef
Administrator1 <admin@owners-mbp.lan>
2016-02-20 14:15:54 -0800

    Problems with glutLoop...

--- skeet/frame.h
+++ skeet/frame.h
@@ -15,6 +15,29 @@
 #include "bullet.h"
 #include "banner.h"
 
+#define LINUX
+//#define MAC_XCODE
+//#define WIN_VISUAL_STUDIO
+
+#ifdef MAC_XCODE
+#include <openGL/gl.h>    // Main OpenGL library
+#include <GLUT/glut.h>    // Second OpenGL library
+#endif // MAC_XCODE
+
+#ifdef LINUX
+//#include <GL/gl.h>    // Main OpenGL library
+//#include <GL/glut.h>  // Second OpenGL library
+#endif // LINUX
+
+#ifdef WIN_VISUAL_STUDIO
+#include <stdio.h>
+#include <stdlib.h>
+#include <glut.h>           // OpenGL library we copied
+#include <ctime>            // for ::Sleep();
+#include <Windows.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#endif // WIN_VISUAL_STUDIO
 
 class Frame
 {
@@ -29,6 +52,10 @@ class Frame
    private:
       //Member Functions
       void draw();
+      void draw(void (*callBack)(const Frame *, void *), void *p);
+   
+      static void *p;                   // for client
+      static void (*callBack)(const Frame *, void *);
 };
 
 #endif /* frame_h */
--- skeet/main.cpp
+++ skeet/main.cpp
@@ -9,6 +9,45 @@
 #include <iostream>
 #include "frame.h"
 
+/*************************************
+ * All the interesting work happens here, when
+ * I get called back from OpenGL to draw a frame.
+ * When I am finished drawing, then the graphics
+ * engine will wait until the proper amount of
+ * time has passed and put the drawing on the screen.
+ **************************************/
+void callBack(const Frame *pFrame, void * p)
+{
+   /*
+   Ball * pBall = (Ball *)p;  // cast the void pointer into a known type
+   
+   // move the polygon
+   if (pFrame->isRight()) //keystrokes//
+      pBall->pt.addX(1);  //change to gun//
+   if (pFrame->isLeft())
+      pBall->pt.addX(-1);
+   if (pFrame->isUp())
+      pBall->pt.addY(1);
+   if (pFrame->isDown())
+      pBall->pt.addY(-1);
+   
+   // use the space bar to change the number of sides.
+   if (pFrame->isSpace())
+      pBall->sides++;
+   if (pBall->sides == 20)
+      pBall->sides = 2;
+   
+   // rotate constantly
+   pBall->rotation++;
+   
+   // draw
+   drawPolygon(pBall->pt,      //position//
+               20,             //radius  //
+               pBall->sides    //segments//,
+               pBall->rotation //rotation//);
+   */
+}
+
 int main(int argc, const char * argv[])
 {
    Frame mainFrame;
@@ -16,4 +55,11 @@ int main(int argc, const char * argv[])
    // insert code here...
    std::cout << "Hello, World!\n";
     return 0;
+   
+   //Frame ourFrame(argc, argv, "Test", Point(-200, 200), Point(200, -200));    // initialize OpenGL
+   Frame ourFrame();    // initialize OpenGL
+   //Ball ball;                           // initialize the game state
+   ourFrame.run(callBack, &ball);             // set everything into action
+   
+   return 0;
 }
