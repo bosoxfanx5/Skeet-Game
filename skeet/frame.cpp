fb5936847f5bdce1a2a5677884acd35802da5b79
twoodd <twoodworth005@gmail.com>
2016-02-20 16:58:06 -0600

    commented out Linus and XCode define lines

--- skeet/frame.cpp
+++ skeet/frame.cpp
@@ -8,5 +8,38 @@
 
 #include "frame.h"
 
+//#define LINUX
+//#define MAC_XCODE
+#define WIN_VISUAL_STUDIO
+
+#ifdef MAC_XCODE
+#include <openGL/gl.h>    // Main OpenGL library
+#include <GLUT/glut.h>    // Second OpenGL library
+#endif // MAC_XCODE
+
+#ifdef LINUX
+//#include <GL/gl.h>    // Main OpenGL library
+//#include <GL/glut.h>  // Second OpenGL library
+#endif // LINUX
+
+#ifdef WIN_VISUAL_STUDIO
+#include <stdio.h>
+#include <stdlib.h>
+#include <glut.h>           // OpenGL library we copied
+#include <ctime>            // for ::Sleep();
+#include <Windows.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#endif // WIN_VISUAL_STUDIO
+
 //Member Functions
-void draw();
+void Frame::draw(void (*callBack)(const Frame *, void *), void *p)
+{
+   // setup the callbacks
+   this->p = p;
+   this->callBack = callBack;
+      
+   glutMainLoop();
+      
+   return;
+}
